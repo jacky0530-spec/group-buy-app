dec9db798865b86a5b1f893c07e5ecfde432639e
jacky0530-spec <jacky0530@gmail.com>
2026-08-17 06:53:19 +0800

    Bump version for iPad print stability fix

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import { ShoppingBag, Users, ShoppingCart, BarChart2, Home, Menu, X, UserCog, Cl
 import { useEffect, useState } from 'react'
 import { UserMenu } from './AuthGuard'
 
-const APP_VERSION = 'v2026.08.17.2'
+const APP_VERSION = 'v2026.08.17.3'
 
 const NAV = [
   { to: '/', icon: Home, label: '首頁', end: true },
