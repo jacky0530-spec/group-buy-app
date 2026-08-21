127d0b10961e282cf194efa4686a4841216cdb0c
jacky0530-spec <jacky0530@gmail.com>
2026-08-21 22:42:02 +0800

    Load prominent report tab styles

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './responsive.css'
 import './mobile-sidebar-fix.css'
 import './pending-report-modes.css'
 import './batch-order-highlight.css'
+import './report-tabs-highlight.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
