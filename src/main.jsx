795b25450dc6ea09316e87284ec5ec44528c75a5
jacky0530-spec <jacky0530@gmail.com>
2026-08-21 17:39:40 +0800

    Make pending report query modes visually distinct (#23)
    * Make pending report query modes visually distinct
    
    * Load pending report mode styles
    
    * Bump version for clearer report modes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './responsive.css'
 import './mobile-sidebar-fix.css'
+import './pending-report-modes.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
