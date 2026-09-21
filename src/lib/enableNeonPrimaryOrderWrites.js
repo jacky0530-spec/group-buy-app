84bb718efaa8ff5d20a4a0adf9af613da2d60e5f
jacky0530-spec <jacky0530@gmail.com>
2026-09-21 15:57:21 +0800

    V68: expose batch supplier unpaid correction

--- src/lib/enableNeonPrimaryOrderWrites.js
+++ src/lib/enableNeonPrimaryOrderWrites.js
@@ -70,6 +70,7 @@ if(!globalThis[INSTALLED]){
   OrdersAPI.applyRefund=async (id,{amount,note=''}={})=> (await neonOrdersRuntime('apply_refund',{id,amount,note}))?.result
   OrdersAPI.clearRefunds=async id=> (await neonOrdersRuntime('clear_refunds',{id}))?.result
   OrdersAPI.correctSupplierState=async (id,{item_index=null,reset_arrival=false}={})=> (await neonOrderStatusRuntime('correct_supplier_state',{id,item_index,reset_arrival}))?.result
+  OrdersAPI.batchCorrectSupplierPayments=async (ids=[])=> (await neonOrderStatusRuntime('correct_supplier_state_batch',{ids:[...new Set((ids||[]).filter(Boolean))]}))?.result
   OrdersAPI.updateArrival=async function(id,items){
     return (await neonOrdersRuntime('update_arrival',{id,items}))?.result
   }
